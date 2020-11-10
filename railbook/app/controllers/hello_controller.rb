4e15d1cbe0b73ddc0fd77afcb98da96e1994f7d4
TAKAHARA <mwl9dv@gmail.com>
2020-11-10 20:25:28 +0900

    listのビューとルートを定義

--- railbook/app/controllers/hello_controller.rb
+++ railbook/app/controllers/hello_controller.rb
@@ -5,4 +5,7 @@ class HelloController < ApplicationController
   def view
     @msg = 'Hello, world!'
   end
+  def list
+    @books = Book.all
+  end
 end
--- railbook/config/routes.rb
+++ railbook/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'hello/index', to: 'hello#index'
   get 'hello/view'
+  get 'hello/list'
 end
